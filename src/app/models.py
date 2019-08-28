18a5572f4ef6634c6d55b8f36ab9f4656af32251
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2019-08-29 01:00:32 +0200

    Created 'Anime' model.

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Anime(models.Model):
+
+    name = models.CharField(max_length=100)
+    original_manga = models.CharField(max_length=100)
+    last_adapted_chapter = models.IntegerField()
+    likes = models.IntegerField()
+    update_date = models.DateTimeField()
+
