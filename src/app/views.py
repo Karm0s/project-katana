1ffbe805bb6fc61d5bdde6fd99b8877a30f40f0b
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2019-08-30 01:51:11 +0200

    Created html base file.

--- src/app/views.py
+++ src/app/views.py
@@ -12,3 +12,9 @@ class IndexListView(ListView):
     def get_queryset(self):
         return Anime.objects.all()
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
+    
+    
